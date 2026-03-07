6e81592db73179c5531f4f748c3868cb1e244f03
Fabien Couillard <couillard.fabien+dev@gmail.com>
2026-03-07 11:20:43 +0100

    Apply module: spring-boot

--- src/test/java/io/mend/seed/IntegrationTest.java
+++ src/test/java/io/mend/seed/IntegrationTest.java
@@ -5,18 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.DisplayNameGeneration;
-import org.springframework.security.test.context.support.WithMockUser;
-import io.mend.seed.shared.authentication.infrastructure.primary.TestSecurityConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@WithMockUser
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @DisplayNameGeneration(ReplaceCamelCase.class)
-@SpringBootTest(classes = { Seed4jSampleApplicationApp.class, TestSecurityConfiguration.class })
+@SpringBootTest(classes = { Seed4jSampleApplicationApp.class })
 public @interface IntegrationTest {
   @AliasFor(annotation = SpringBootTest.class)
   String[] properties() default {};
