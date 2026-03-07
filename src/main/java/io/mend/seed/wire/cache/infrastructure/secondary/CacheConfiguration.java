b547de5152b46e56b975f3f7510497e6e3af493b
Fabien Couillard <couillard.fabien+dev@gmail.com>
2026-03-07 11:27:26 +0100

    Apply module: spring-boot-cache

---
+++ src/main/java/io/mend/seed/wire/cache/infrastructure/secondary/CacheConfiguration.java
@@ -0,0 +1,8 @@
+package io.mend.seed.wire.cache.infrastructure.secondary;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+class CacheConfiguration {}
