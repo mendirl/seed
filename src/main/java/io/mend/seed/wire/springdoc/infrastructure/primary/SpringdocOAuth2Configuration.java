3985365286c35b4c84ab75de34c4ead4ca6b5ba6
Fabien Couillard <couillard.fabien+dev@gmail.com>
2026-03-07 11:27:27 +0100

    Apply module: springdoc-oauth2

---
+++ src/main/java/io/mend/seed/wire/springdoc/infrastructure/primary/SpringdocOAuth2Configuration.java
@@ -0,0 +1,40 @@
+package io.mend.seed.wire.springdoc.infrastructure.primary;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Arrays;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.mend.seed.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@Configuration
+@AutoConfigureBefore(SpringdocConfiguration.class)
+@ExcludeFromGeneratedCodeCoverage(reason = "Not called by integration tests")
+class SpringdocOAuth2Configuration {
+
+  @Value("${springdoc.oauth2.authorization-url}")
+  private String authorizationUrl;
+
+  @Bean
+  GlobalOpenApiCustomizer oauthOpenApi() {
+    return openApi ->
+      openApi
+        .components(oauthComponents(openApi.getComponents()))
+        .addSecurityItem(new SecurityRequirement().addList("security_auth", Arrays.asList("seed4j")));
+  }
+
+  private Components oauthComponents(Components existingComponents) {
+    return existingComponents.addSecuritySchemes(
+      "security_auth",
+      new SecurityScheme()
+        .type(SecurityScheme.Type.OAUTH2)
+        .flows(new OAuthFlows().implicit(new OAuthFlow().authorizationUrl(authorizationUrl)))
+    );
+  }
+}
